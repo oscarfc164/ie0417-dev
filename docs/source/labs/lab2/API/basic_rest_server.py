7fc84c3cbf35c5bb6728d6b9367c085276b45bf2
oscarfc164 <ofallas164@gmail.com>
2022-05-14 21:18:34 -0600

    Adding a functional version of API Needs to implement it w/ eieManager

--- docs/source/labs/lab2/API/basic_rest_server.py
+++ docs/source/labs/lab2/API/basic_rest_server.py
@@ -3,6 +3,7 @@ REST API using FASTAPI for eieManager
 """
 
 from ntpath import join
+from unicodedata import name
 from fastapi import FastAPI, Body
 from random import randint
 from typing import Optional
@@ -11,29 +12,17 @@ import json
 
 
 class Device(BaseModel):
+    identificator: str
     name: str
-    type_device: Optional[str] = None
+    type_device: str
+    commands_s : list
     host_port: str
 
 app = FastAPI()
 
-devices_desc = []
-with open('devices.json') as file:
-    dev = json.load(file)
 
-    for devs in dev['devices']:
-        lista = (devs['name'], devs['tipo'])
-
-        devices_desc.append(lista)
-
-devices = {
-    name: Device(
-        name=name,
-        tipo=tipo,
-        host_port = "127.0.0.1" + ":" + str(randint(0000,9999))
-    )
-    for name, tipo in devices_desc
-}
+with open("devices.json") as filename:
+    devices = json.load(filename)
 
 
 
@@ -44,8 +33,15 @@ def create_device(device: Device):
 
     :param device, new device to register.
     """
-    devices[device.name] = device
-    return device
+    num_inde = len(devices.keys())
+    indentificator = "DEV" + str(num_inde + 1)
+
+    devices[indentificator] = [
+        {"name": device.name,
+        "tipo": device.type_device,
+        "commands": device.commands_s}
+    ]
+
 
 
 @app.get("/devices/")
@@ -58,24 +54,26 @@ def read_devices(first: int = 0, limit: int = 10):
     """
     devices_list = [device for device in devices.values()]
     return devices_list[first : first + limit]
+    
 
 
 @app.delete("/devices/{device_name}")
-def delete_item(device_name: str, status_code=204):
+def delete_item(indetificador: str, status_code=204):
     """
     Unregister and delete item.
 
     :param str device_name: Name of the device to delete.
     :param int status_code: Default HTTP status code to return.
     """
-    del devices[device_name]
+    del devices[indetificador]
+    
 
 
 @app.get("/devices/{device_name}")
-def read_item(device_name: str):
+def read_item(identificator: str):
     """
     Get specific device from name.
 
     :param str item_name: Name of the device to get.
     """
-    return devices[device_name]
+    return devices[identificator]
