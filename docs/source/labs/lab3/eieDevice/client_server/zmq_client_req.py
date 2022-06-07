906c55bd769bb72183afa93a7b39d4f89fe98a49
oscarfc164 <ofallas164@gmail.com>
2022-06-06 20:48:30 -0600

    Transport server final commit

--- docs/source/labs/lab3/eieDevice/client_server/zmq_client_req.py
+++ docs/source/labs/lab3/eieDevice/client_server/zmq_client_req.py
@@ -7,22 +7,36 @@ This is the client script for the zmq_server_rep_pthread program (@ examples/c/z
 import time
 import zmq
 import struct
+import json
 
+def config_json(file):
+    file_name = open('device_cfg.json')
+    payload = json.load(file_name)
+    file_name.close()
+    payload = json.dumps(payload)
+    payload = bytes(payload, 'utf-8')
+    return payload
 
 def main():
-    port = 5555
+    commands = ["add", "destroy"]
+    payload = config_json('device_cfg.json')
+    payload_size = len(payload)
+
+    port = 8888;
     context = zmq.Context()
     print("Connecting to server...")
     client = context.socket(zmq.REQ)
     with client.connect(f"tcp://localhost:{port}"):
-        cmd_req1 = 
-        req = struct.pack('<BI', req_type, req_val)
-        client.send(req)
+        for command in commands:
+            command = bytes(command, "utf-8")
+            header = struct.pack("< 100s i", command, payload_size)
+            print(header, payload)
+            client.send(header+payload)
 
             # Receive response
-        rep = client.recv()
-        rep_val_a, rep_val_b = struct.unpack('<QB', rep)
-        print(f"Received response [val_a: {rep_val_a}, val_b: {rep_val_b}]")
+        ans = client.recv()
+        answer, answer_size = struct.unpack('<100s i 100s', ans)
+        print(f"Received response [command: {answer}, val_b: {answer_size}]")
 
 
 if __name__ == "__main__":
